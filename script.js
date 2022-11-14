f4b98801ec5330d77a27c815cf222765b612d6c8
Wahaj Ali <wahaj.ali96@yahoo.com>
2022-11-14 22:32:44 +0500

    updated menu links text
    in this commit
    - edit the font size and font color for the menu links for mobile version

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ menuIcon.addEventListener('click', () => {
   menuIcon.classList.toggle('active');
   mainMenu.classList.toggle('active');
 });
-document.querySelectorAll(".header-btn").forEach( (n) => n.addEventListener("click", () => {
+document.querySelectorAll('.header-btn').forEach((n) => n.addEventListener('click', () => {
   menuIcon.classList.remove('active');
   mainMenu.classList.remove('active');
 }));
