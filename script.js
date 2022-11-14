3a4bfd49d4e181917b11db0793369f401b628345
Wahaj Ali <wahaj.ali96@yahoo.com>
2022-11-14 23:56:41 +0500

    Made required changes
    in this commit
    - remove logo when in the menu of mobile version as required by the reviewer.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const menuIcon = document.querySelector('.menu-icon');
 const mainMenu = document.querySelector('.main-menu');
+const myLogo = document.querySelector('.mylogo')
 
 menuIcon.addEventListener('click', () => {
   menuIcon.classList.toggle('active');
   mainMenu.classList.toggle('active');
+  myLogo.classList.toggle('disappear')
 });
 document.querySelectorAll('.header-btn').forEach((n) => n.addEventListener('click', () => {
   menuIcon.classList.remove('active');
