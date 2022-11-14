114b33047573c7b81a273a0fc47a570bdf2a6383
Wahaj Ali <wahaj.ali96@yahoo.com>
2022-11-14 22:21:56 +0500

    removed linter errors
    in this commit
    - removed linter errors from styles.css and script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const menuIcon = document.querySelector(".menu-icon");
-const mainMenu = document.querySelector(".main-menu");
+const menuIcon = document.querySelector('.menu-icon');
+const mainMenu = document.querySelector('.main-menu');
 
-menuIcon.addEventListener("click", () => {
-    menuIcon.classList.toggle("active");
-    mainMenu.classList.toggle("active");
-  })
-  
-  document.querySelectorAll(".header-btn").forEach(n => n.addEventListener("click", () => {
-    menuIcon.classList.remove("active");
-    mainMenu.classList.remove("active");
-  }))
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('active');
+  mainMenu.classList.toggle('active');
+});
+document.querySelectorAll(".header-btn").forEach( (n) => n.addEventListener("click", () => {
+  menuIcon.classList.remove('active');
+  mainMenu.classList.remove('active');
+}));
