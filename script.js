2bdbc5afaa59216d0bf832dc2d0e8ca83b58b06a
Utsargo Roy <royutsargo@gmail.com>
2022-11-16 15:11:48 +0600

    Made the tags dynamically pulled from an array

--- script.js
+++ script.js
@@ -26,18 +26,14 @@ const cards = [{
   title: 'Multi-Post-Stories',
   description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
   img: '/attributes/workimage.png',
-  tech1: 'HTML',
-  tech2: 'Bootstrap',
-  tech3: 'Ruby',
+  techs: ['HTML', 'Bootstrap', 'Ruby'],
   btnText: 'See Project',
 },
 {
   title: 'Profesional Art Printing Data More',
   description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
   img: '/attributes/workimage.png',
-  tech1: 'HTML',
-  tech2: 'Bootstrap',
-  tech3: 'Ruby',
+  techs: ['HTML', 'Bootstrap', 'Ruby'],
   btnText: 'See Project',
   id: 1,
   uniqueId: 'prcjt1'
@@ -46,9 +42,7 @@ const cards = [{
   title: 'Multi-Post-Stories',
   description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
   img: '/attributes/prjct2.png',
-  tech1: 'HTML',
-  tech2: 'Bootstrap',
-  tech3: 'Ruby',
+  techs: ['HTML', 'Bootstrap', 'Ruby'],
   btnText: 'See Project',
   id: 2,
   uniqueId: 'prcjt2'
@@ -57,9 +51,7 @@ const cards = [{
   title: 'Multi-Post-Stories',
   description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
   img: '/attributes/prjct3.png',
-  tech1: 'HTML',
-  tech2: 'Bootstrap',
-  tech3: 'Ruby',
+  techs: ['HTML', 'Bootstrap', 'Ruby'],
   btnText: 'See Project',
   id: 3,
   uniqueId: 'prcjt3'
@@ -68,9 +60,7 @@ const cards = [{
   title: 'Profesional Art Printing Data More',
   description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
   img: '/attributes/workimage.png',
-  tech1: 'HTML',
-  tech2: 'Bootstrap',
-  tech3: 'Ruby',
+  techs: ['HTML', 'Bootstrap', 'Ruby'],
   btnText: 'See Project',
   id: 4,
   uniqueId: 'prcjt4'
@@ -79,9 +69,7 @@ const cards = [{
   title: 'Multi-Post-Stories',
   description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
   img: '/attributes/prjct2.png',
-  tech1: 'HTML',
-  tech2: 'Bootstrap',
-  tech3: 'Ruby',
+  techs: ['HTML', 'Bootstrap', 'Ruby'],
   btnText: 'See Project',
   id: 5,
   uniqueId: 'prcjt5'
@@ -90,9 +78,7 @@ const cards = [{
   title: 'Multi-Post-Stories',
   description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
   img: '/attributes/prjct3.png',
-  tech1: 'HTML',
-  tech2: 'Bootstrap',
-  tech3: 'Ruby',
+  techs: ['HTML', 'Bootstrap', 'Ruby'],
   btnText: 'See Project',
   id: 6,
   uniqueId: 'prcjt6'
@@ -103,6 +89,13 @@ function displayCards() {
   let result = '';
 
   cards.filter((card) => card.id > 0).forEach((card) => {
+    let techList = ''; // list of tags as li element
+    const techTags = card.techs;
+    //   populate tag list iterating over tags array
+    for (let x = 0; x < techTags.length; x += 1) {
+      techList += `<li class="lang">${techTags[x]}</li>`;
+    }
+    
     result += `
 
     <section class="project" id="${card.uniqueId}">
@@ -110,9 +103,7 @@ function displayCards() {
             <p class="project_detail">${card.description}</p>
             <article class="lang_container_2">
                 <ul style="list-style-type:none;" class="list">
-                    <li id="lang1">${card.tech1}</li>
-                    <li id="lang2">${card.tech2}</li>
-                    <li id="lang3">${card.tech3}</li>
+                    ${techList}
                 </ul>
             </article>
             <button type="submit" class="checkout-button button_interactions" id="projects-btn2" name="checkout-button"
@@ -132,15 +123,19 @@ cards.forEach((card) => {
     btn[i].addEventListener('click', () => {
       if (i === ((card.id))) {
         popupWin.style.display = 'block';
+        let modalTechs = '';
+        const modalTags = card.techs;
+    //   populate tag list iterating over tags array
+    for (let x = 0; x < modalTags.length; x += 1) {
+      modalTechs += `<li class="lang">${modalTags[x]}</li>`;
+    }
         const show = `<div class="content-card-wrap">
         <div class="content-card">
          <div class="close-button card-x"><img src="./attributes/cross.png" alt="X"></div>
          <div class="cc-h-wrap">
            <h2 class="card-h">${card.title}</h2>
            <ul class="card-lang-wrapper">
-             <li>${card.tech1}</li>
-             <li>${card.tech2}</li>
-             <li>${card.tech3}</li>
+           ${modalTechs}
            </ul>
          </div>
          <div class="card-content">
